3ed33ce98a393a4b3b811dc328a24bbe03cc1f17
vishesh agarwal <162690265+vishesh003@users.noreply.github.com>
2026-09-25 15:27:44 +0530

    Add: buy-and-sell-crypto - submission-4

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-4.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int minprice=prices[0];
+        int maxprofit=0;
+        for(int i=0;i<prices.size();i++){
+            
+            minprice=min(prices[i],minprice);
+            maxprofit=max(maxprofit,prices[i]-minprice);
+        }
+        return maxprofit;
+    }
+};
